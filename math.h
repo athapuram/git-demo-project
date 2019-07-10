ac06dfccd0f0ebe9e75451da6618d6882b3e2ac8
Praveen Reddy A <athapurampraveen@gmail.com>
2019-07-10 09:50:40 +0530

    added function declarations
    added sub() and multiplication() function declaration to header file.

--- math.h
+++ math.h
@@ -9,3 +9,15 @@
  * return value: sum of two input arguments
  */
   int add (int, int);
+
+/* Fun: int sub (int a, int b)
+ * arguments: two integer variables
+ * return value: substracted value of a-b
+ */
+  int sub (int, int);
+
+/* Fun: int multiplication (int a, int b)
+ * arguments: two integer variables
+ * return value: multiplication value of two input arguments
+ */
+  int multiplication (int, int);
