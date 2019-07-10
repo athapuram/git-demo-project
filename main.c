d917a74de7cb0931325987db1c0c882d8404fe86
Praveen Reddy A <athapurampraveen@gmail.com>
2019-07-10 09:42:53 +0530

    added calculator program in main.c
    calculator program is written by using math.h and stdlib.h libraries.

--- main.c
+++ main.c
@@ -1,7 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "math.h"
 
 int mani()
 {
-	printf("Hello World!!");
+	int a, b;
+	char input;
+	while(1){
+		system("cls");
+		printf("Running Calculator programing\n");
+		printf("Press:\n");
+		printf("+ for Addition\n");
+		printf("- for Substraction\n");
+		printf("* for multiplication\n");	
+		printf("x for Exiting from program\n");
+		if(input == 'x' || input == 'X')
+		{
+			return 0;
+		}
+			
+		printf("Enter integer value a = ");
+		scanf("%d", &a);
+		printf("Enter integer value b = ");
+		scanf("%d", &b);	
+		switch(input)
+		{
+			case '+':
+				printf("result of a+b = %d", add(a+b));
+				break;
+			case '-':
+				printf("result of a-b = %d", sub(a-b));
+				break;
+			case '*':
+				printf("result of a*b = %d", multiplication(a*b));
+				break;
+			default:
+				printf("Invalid operation rquested\n");
+				break;
+		}
+		printf("press enter, to try again\n");
+		getch();	
+	}
 	return 0;
 }
